1249c6131773a2241acb4b994b953a12ff0df75c
Lain5999 <149149279+Lain5999@users.noreply.github.com>
2023-11-27 12:12:00 +0100

    Add files via upload

---
+++ ExerciseNovember27-appendchild/script.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    var nuevoparrafo=document.createElement("p");
+    var texto=document.createTextNode("holaaas");
+    nuevoparrafo.appendChild(texto);
+    document.body.appendChild(nuevoparrafo);
+    
+}  
